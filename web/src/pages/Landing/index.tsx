244ed3ee10e220a870c2fd10c26af715d59b8d28
Douglas Mendes <capudougmor@gmail.com>
2020-08-03 21:25:15 -0300

    Create component header

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import './styles.css'
 
@@ -24,14 +25,14 @@ function Landing() {
           className="hero-image" 
         />
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="study" className="study">
             <img src={ studyIcon} alt="Estudar"/>
             Estudar
-          </a>
-          <a href="" className="gice-classes">
+          </Link>
+          <Link to="give-classes" className="gice-classes">
             <img src={ giveClassesIcon} alt="Dar aulas"/>
             Dar aulas
-          </a>
+          </Link>
         </div>
         <span className='total-connections' > 
           Total de 200 conexões já realizadas <img src={ purpleHeartIcon } alt="coração roxo"/>
