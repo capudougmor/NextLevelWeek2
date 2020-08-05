390c8b98f5a9d9c46358834467f7def89373912d
Douglas Mendes <capudougmor@gmail.com>
2020-08-04 21:05:06 -0300

    create api

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,68 +1,17 @@
 import express from 'express'
-import db from './database/connection'
-import convertHoursToMinutes from './utils/convertHoursToMinutes'
+
+import ClassesController from './controllers/ClassesController'
+import ConnectionsController from './controllers/ConnectionsController'
 
 const routes = express.Router()
+const classesControllers = new ClassesController()
+const connectionController = new ConnectionsController()
 
-interface ScheduleItems {
-  week_day: number;
-  from: string;
-  to: string;
-}
+routes.get('/classes', classesControllers.index)
+routes.post('/classes', classesControllers.create)
 
-routes.post('/classes', async (req, res) => {
-  const {
-    name,
-    avatar,
-    whatsapp,
-    bio,
-    subject,
-    cost,
-    schedule
-  } = req.body
-
-  const trx = await db.transaction()
-
-  try {
-    const insertedUsersIds = await trx('users').insert({
-      name,
-      avatar,
-      whatsapp,
-      bio
-    })
-  
-    const user_id = insertedUsersIds[0]
-  
-    const insertedClassesIds = await trx('classes').insert({
-      subject,
-      cost,
-      user_id
-    })
-  
-    const class_id = insertedClassesIds[0]
-  
-    const classSchedule = schedule.map((scheduleItem: ScheduleItems) => {
-      return {
-        class_id,
-        week_day: scheduleItem.week_day,
-        from: convertHoursToMinutes(scheduleItem.from),
-        to: convertHoursToMinutes(scheduleItem.to),
-      }
-    })
-  
-    await trx('class_schedule').insert(classSchedule)
-  
-    await trx.commit()
-  
-    return res.status(201).send("ok")
-  } catch (err) {
-    await trx.rollback()
-
-    return res.status(400).json({
-      error: 'Unexpected error while creating new class'
-    })
-  }
-})
+routes.post('/connections', connectionController.create)
+routes.get('/connections', connectionController.index)
 
 export default routes
 // console.log(data)
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
-
-
+app.use(cors)
 
 console.log("ok")
 
